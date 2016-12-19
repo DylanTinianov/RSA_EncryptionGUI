0dfd275a4ed5d376bd780a5160c14e039b9fbdb9
Dylan Tinianov <dylantinianov@gmail.com>
2016-12-18 23:46:43 -0500

    Unit tests

---
+++ unit_tests/test_encryption.py
@@ -0,0 +1,16 @@
+import unittest
+#from rsa_encryption_gui.host import network_socket
+#from rsa_encryption_gui.client import network_client
+
+
+class EncryptionTests(unittest.TestCase):
+
+    def test_host(self):
+        assert 1 == 1
+
+    # def test_client(self):
+        # network_client.run()
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
